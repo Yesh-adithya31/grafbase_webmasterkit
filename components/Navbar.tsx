9d68f43ca06d5df1ba63d7037a257e3c3b6c1ca2
Yesh-adithya31 <57474408+Yesh-adithya31@users.noreply.github.com>
2023-07-15 22:34:03 +0530

    Finalization

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,51 @@
-import { NavLinks } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import Authproviders from './Authproviders';
+import Image from "next/image";
+import Link from "next/link";
 
-const Navbar = () => {
-    const session = {};
+import { getCurrentUser } from "@/lib/session";
+
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
+import { NavLinks } from "@/constants";
+import AuthProviders from "./Authproviders";
+
+const Navbar = async () => {
+  const session = await getCurrentUser()
+
   return (
-    <nav className="flexBetween navbar">
-        <div className="flex-1 flexStart gap-10">
-            <Link href="/">
-                <Image 
-                    src="/logo.svg"
-                    width={165}
-                    height={43}
-                    alt="Template Breakers"
-                />
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            width={116}
+            height={43}
+            alt='logo'
+          />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
             </Link>
-            <ul className="xl:flex hidden text-small gap-7">
-                {NavLinks.map((link) => (
-                    <Link href={link.href} key= {link.key}>
-                        {link.text}
-                    </Link>
-                ))}
-            </ul>
-        </div>
+          ))}
+        </ul>
+      </div>
 
-        <div className="flexCenter gap-4">
-            {session ? (
-                <>
-                    UserPhoto
-                    <Link href="/create-project">
-                        Share Work
-                    </Link>
-                </>
-            ) : (
-                <Authproviders />
-            )}
-        </div>
+      <div className='flexCenter gap-4'>
+        {session?.user ? (
+          <>
+            <ProfileMenu session={session} />
+
+            <Link href="/create-project">
+              <Button title='Share work' />
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, config, auth } from '@grafbase/sdk';
 
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
@@ -8,8 +9,11 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
 })
 
+// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
@@ -18,6 +22,9 @@ const Project = g.model('Project', {
   githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
 })
 
 const jwt = auth.JWT({
@@ -32,3 +39,4 @@ export default config({
     rules: (rules) => rules.private()
   },
 })
+
