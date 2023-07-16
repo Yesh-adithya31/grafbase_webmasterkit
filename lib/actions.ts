db09cfdb3e8747480b1679ef2b9b070b1e503d2d
Yesh-adithya31 <57474408+Yesh-adithya31@users.noreply.github.com>
2023-07-16 17:40:19 +0530

    Acion Changes

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,10 +3,10 @@ import { GraphQLClient } from "graphql-request";
 import { createProjectMutation, createUserMutation, deleteProjectMutation, updateProjectMutation, getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery } from "@/graphql";
 import { ProjectForm } from "@/common.types";
 
-// const isProduction = process.env.NODE_ENV === 'production';
-const apiUrl = process.env.NEXT_PUBLIC_GRAFBASE_API_URL!;
-const apiKey = process.env.NEXT_PUBLIC_GRAFBASE_API_KEY!;
-const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+const isProduction = process.env.NODE_ENV === 'production';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
 const client = new GraphQLClient(apiUrl);
 
